c08b3860c35a6a81503cad6a10e134cd95079b66
woosolkim <woosol2da@gmail.com>
2022-03-06 00:49:56 +0900

    0304 study

--- src/main/java/com/jojoldu/book/springboot/web/PostApiController.java
+++ src/main/java/com/jojoldu/book/springboot/web/PostApiController.java
@@ -2,11 +2,11 @@ package com.jojoldu.book.springboot.web;
 
 
 import com.jojoldu.book.springboot.domain.posts.PostsService;
+import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
+import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,5 +19,18 @@ public class PostApiController {
         return postsService.save(requestDto);
     }
 
+
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+
+        return postsService.findById(id);
+    }
 
 }
